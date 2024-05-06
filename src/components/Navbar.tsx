021de1626b842361310dbb68d50445d70d58e150
lukres <lukres006@gmail.com>
2024-05-06 10:42:52 +0300

    Small changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,8 +35,8 @@ export default function Navbar() {
                 <nav style={{ boxShadow: '0 0 30px 2.5px #0a0a0a' }} className="flex h-14 max-w-[46rem] w-screen bg-gradient-to-br from-primary/90 to to-secondary/90 backdrop-blur-md rounded-lg border-1 border-accent px-4">
                     <div className="flex flex-row items-center justify-between w-full">
                         <div className="flex flex-row gap-2 items-center">
-                            <img draggable={false} className="h-10 w-10" src="/me.png" />
-                            <button onClick={() => window.location.href = '/'} className="p-2 duration-300 text-text text-lg font-medium hover:bg-secondary font-leaguespartan rounded-md">
+                            <img onClick={() => window.location.href = 'https://lukres.dev'} draggable={false} className="h-10 w-10" src="/me.png" />
+                                <button onClick={() => window.location.href = '/'} className="p-2 duration-300 text-text text-lg font-medium hover:bg-secondary font-leaguespartan rounded-md">
                                 Home
                             </button>
                             <button onClick={() => window.location.href = '/history'} className="p-2 duration-300 text-text text-lg font-medium hover:bg-secondary font-leaguespartan rounded-md">
