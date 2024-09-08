f2c7ddb3d4afca236e0ab42aacd74b2fa7ec4e1e
LuKres <lukres006@gmail.com>
2024-09-08 14:23:36 +0300

    Test webhook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -193,7 +193,7 @@ export default function Home() {
                 <footer>
                     <div className="h-0.5 w-full rounded-lg bg-gradient-to-r from-secondary via-accent to-secondary"/>
                     <h2 className="font-leaguespartan text-center font-semibold text-base text-text pt-2">
-                        phishing.lukres.dev - Made with NextJS, TailwindCSS, and ❤ by Luka!
+                        phishing.lukres.dev - Made with NextJS, TailwindCSS, and ❤ by Luka!!
                     </h2>
                 </footer>
             </div>
