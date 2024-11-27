05781a2571aadac8b6f72836738356a1372599ee
LuKres <lukres006@gmail.com>
2024-11-27 11:04:46 +0300

    ecosystem.config.js added

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    apps: [
+        {
+            name: 'phishing-detector-web',
+            script: 'npm',
+            args: 'start',
+            cwd: './',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3001
+            },
+            exp_backoff_restart_delay: 100,
+            max_restarts: 10,
+            error_file: 'logs/web-err.log',
+            out_file: 'logs/web-out.log',
+            time: true
+        },
+        {
+            name: 'phishing-detector-ml',
+            script: 'python3',
+            args: 'ml/server.py',
+            interpreter: 'python3',
+            cwd: './',
+            env: {
+                PYTHONUNBUFFERED: 'true',
+                ML_SERVER_PORT: '5002'
+            },
+            exp_backoff_restart_delay: 100,
+            max_restarts: 10,
+            error_file: 'logs/ml-err.log',
+            out_file: 'logs/ml-out.log',
+            time: true
+        }
+    ]
+};
